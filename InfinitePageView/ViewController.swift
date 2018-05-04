500ac5b586590a7acbe5a44bd7335258f3fbe362
takumi314 <isospin_symmetry@me.com>
2018-05-04 23:18:23 +0900

    first commit

---
+++ InfinitePageView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InfinitePageView
+//
+//  Created by NishiokaKohei on 2018/05/04.
+//  Copyright © 2018年 Takumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
