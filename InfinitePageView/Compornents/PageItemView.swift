85722d5872d47d71242b7baaeb4ce8ed1a55bee4
takumi314 <isospin_symmetry@me.com>
2018-05-05 00:43:44 +0900

    [feat] make new class

---
+++ InfinitePageView/Compornents/PageItemView.swift
@@ -0,0 +1,15 @@
+//
+//  PageItemView.swift
+//  InfinitePageView
+//
+//  Created by NishiokaKohei on 2018/05/05.
+//  Copyright © 2018年 Takumi. All rights reserved.
+//
+
+import UIKit
+
+class PageItemView: UIView {
+
+
+    
+}
