eebb46beb2be9500cd8a57286631796298b37aed
takumi314 <isospin_symmetry@me.com>
2018-05-05 00:53:55 +0900

    [feat] make new struct

---
+++ InfinitePageView/Compornents/PageMenuConfiguration.swift
@@ -0,0 +1,9 @@
+//
+//  PageMenuConfiguration.swift
+//  InfinitePageView
+//
+//  Created by NishiokaKohei on 2018/05/05.
+//  Copyright © 2018年 Takumi. All rights reserved.
+//
+
+import Foundation
