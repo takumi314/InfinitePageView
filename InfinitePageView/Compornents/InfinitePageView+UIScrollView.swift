7963a6e455c39b5dd94921efdcbc6d4bef726f37
takumi314 <isospin_symmetry@me.com>
2018-05-05 11:16:42 +0900

    [feat] make new class extension

---
+++ InfinitePageView/Compornents/InfinitePageView+UIScrollView.swift
@@ -0,0 +1,13 @@
+//
+//  InfinitePageView+UIScrollView.swift
+//  InfinitePageView
+//
+//  Created by NishiokaKohei on 2018/05/05.
+//  Copyright © 2018年 Takumi. All rights reserved.
+//
+
+import UIKit
+
+extension InfinitePageView: UIScrollViewDelegate {
+    
+}
