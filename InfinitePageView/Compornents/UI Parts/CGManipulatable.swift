60713bf8413976bd95dd43bf06d7638caf4b802b
takumi314 <isospin_symmetry@me.com>
2018-05-07 23:14:59 +0900

    [feat] make new protocol

---
+++ InfinitePageView/Compornents/UI Parts/CGManipulatable.swift
@@ -0,0 +1,39 @@
+//
+//  CGManipulatable.swift
+//  InfinitePageView
+//
+//  Created by NishiokaKohei on 2018/05/07.
+//  Copyright © 2018年 Takumi. All rights reserved.
+//
+
+import UIKit
+
+public protocol CGManipulatable {
+    var size: CGSize { get }
+    var origin: CGPoint { get }
+    func updateFrame(_ rect: CGRect)
+}
+
+public extension CGManipulatable where Self: PageContentView {
+    public var size: CGSize {
+        return self.frame.size
+    }
+    public var origin: CGPoint {
+        return self.frame.origin
+    }
+    public func updateFrame(_ rect: CGRect) {
+        self.frame = rect
+    }
+}
+
+public extension CGManipulatable where Self: PageItemView {
+    public var size: CGSize {
+        return self.frame.size
+    }
+    public var origin: CGPoint {
+        return self.frame.origin
+    }
+    public func updateFrame(_ rect: CGRect) {
+        self.frame = rect
+    }
+}
